3163a84e690bacb388ed97cb466ce1470c6e3d88
Charlie kehler <charlespkehler@gmail.com>
2019-09-10 10:43:10 -0400

    adddform messages

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./contact.css";
+import NetlifyForm from "react-netlify-form";
 
 const Contact = () => {
   return (
@@ -9,21 +10,34 @@ const Contact = () => {
         Have any questions? Want to hire me? Send a message, I'd love to hear
         from you.
       </div>
-      <form
-        className="contact-container"
-        name="contact"
-        method="post"
-        data-netlify="true"
-      >
-        <input type="hidden" name="contact" value="contact" />
-        <p>Name</p>
-        <input type="text" name="name"></input>
-        <p>Email</p>
-        <input type="text" name="email"></input>
-        <p>Message</p>
-        <textarea type="text" name="message"></textarea>
-        <button type="submit">SUBMIT</button>
-      </form>
+      <NetlifyForm name="Contact Form">
+        {({ loading, error, success }) => (
+          <div>
+            {loading && <div>Loading...</div>}
+            {error && (
+              <div>Your information was not sent. Please try again later.</div>
+            )}
+            {success && <div>Thank you for contacting us!</div>}
+            {!loading && !success && (
+              <form
+                className="contact-container"
+                name="contact"
+                method="post"
+                data-netlify="true"
+              >
+                <input type="hidden" name="contact" value="contact" />
+                <p>Name</p>
+                <input type="text" name="name"></input>
+                <p>Email</p>
+                <input type="text" name="email"></input>
+                <p>Message</p>
+                <textarea type="text" name="message"></textarea>
+                <button type="submit">SUBMIT</button>
+              </form>
+            )}
+          </div>
+        )}
+      </NetlifyForm>
     </div>
   );
 };
