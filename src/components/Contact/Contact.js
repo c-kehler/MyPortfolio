da952a59280cd976c9c1ab0518b79982230ee481
Charlie kehler <charlespkehler@gmail.com>
2019-09-10 10:55:28 -0400

    refactoring form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./contact.css";
-import NetlifyForm from "react-netlify-form";
 
 const Contact = () => {
   return (
@@ -10,34 +9,16 @@ const Contact = () => {
         Have any questions? Want to hire me? Send a message, I'd love to hear
         from you.
       </div>
-      <NetlifyForm name="Contact Form">
-        {({ loading, error, success }) => (
-          <div>
-            {loading && <div>Loading...</div>}
-            {error && (
-              <div>Your information was not sent. Please try again later.</div>
-            )}
-            {success && <div>Thank you for contacting us!</div>}
-            {!loading && !success && (
-              <form
-                className="contact-container"
-                name="contact"
-                method="post"
-                data-netlify="true"
-              >
-                <input type="hidden" name="contact" value="contact" />
-                <p>Name</p>
-                <input type="text" name="name"></input>
-                <p>Email</p>
-                <input type="text" name="email"></input>
-                <p>Message</p>
-                <textarea type="text" name="message"></textarea>
-                <button type="submit">SUBMIT</button>
-              </form>
-            )}
-          </div>
-        )}
-      </NetlifyForm>
+      <form className="contact-container" name="contact" method="post">
+        <input type="hidden" name="form" value="contact" />
+        <p>Name</p>
+        <input type="text" name="name"></input>
+        <p>Email</p>
+        <input type="text" name="email"></input>
+        <p>Message</p>
+        <textarea type="text" name="message"></textarea>
+        <button type="submit">SUBMIT</button>
+      </form>
     </div>
   );
 };
